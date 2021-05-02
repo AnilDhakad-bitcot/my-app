c2ffe72821d87a3e0edc3cfac6cc61e6a498171e
Anil Dhakad <anil@bitcot.com>
2021-05-02 19:08:17 +0530

    hello

--- src/componets/google.js
+++ src/componets/google.js
@@ -4,9 +4,26 @@ const Googlepage =()=>{
     return(
         <div>
             <p>
+    
                 hello Google
             </p>
+            <div>hellio</div>
             <h1>
+                my Dream 
+            </h1> <h1>
+                my Dream 
+            </h1> <h1>
+                my Dream 
+            </h1> <h1>
+                my Dream 
+            </h1> <h1>
+                my Dream 
+            </h1> <h1>
+                my Dream 
+            </h1>
+            <h1>
+                my Dream 
+            </h1> <h1>
                 my Dream 
             </h1>
         </div>

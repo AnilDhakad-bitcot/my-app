bec32dfb7acacc6e6e9e62177559342e6e39afde
Anil Dhakad <anil@bitcot.com>
2021-05-02 18:29:19 +0530

    add new file

---
+++ src/componets/Hello.js
@@ -0,0 +1,15 @@
+import REact from React
+ 
+
+const Data = ()=>{
+    return(
+        <div>
+            <h1>
+                Hello, Anil Dhakad
+            </h1>
+            <p>
+                my name is Anil dhakad
+            </p>
+        </div>
+    )
+}
